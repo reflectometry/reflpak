c51706a88464f35a3d421a22245a7f5d75dfbf7d
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2003-10-28 16:28:13 +0000

    Default to using C99 fenv functions for floating point error control
    git-svn-id: svn+ssh://danse.us/reflpak@137 6e772d39-f918-0410-b64e-d616bd391b2c

--- gj2/fperror.c
+++ gj2/fperror.c
@@ -1,22 +1,25 @@
 /* This code is in the public domain.
 
+Remove error traps, and provide simple access to floating point sticky
+bits.
+
 C usage:
  
     fpreset();
     ...
-    if (fpreset()) { 
+    if (fperror()) { 
        printf("there has been an error since the last call to fperror()\n"); 
     }
 
 FORTRAN usage:
 
-    INTEGER FPERROR
     EXTERNAL FPERROR
-    CALL FPCLEAR
+    INTEGER FPERROR
+    CALL FPRESET
     ...
   
     IF (FPERROR() .NE. 0) THEN
-       WRITE (*,*) "There has been an error since the last call to FP_ERROR"
+       WRITE (*,*) "There has been an error since the last call to FPERROR"
     ENDIF
 
 */
@@ -26,15 +29,22 @@ FORTRAN usage:
 /* Define C and fortran variants. */
 
 #ifndef F77_FUNC
+#ifdef sgi
+#define F77_FUNC(lower,upper) lower ## _
+#else
 #define F77_FUNC(lower,upper) lower ## __
 #endif
+#endif
+
+#define fperrorF77 F77_FUNC(fperror,FPERROR)
+#define fpresetF77 F77_FUNC(fpreset,FPRESET)
 
 void fpreset (void) 
 { 
   fenv_t env; 
   feholdexcept(&env); 
 }
-void F77_FUNC(fpreset,FPRESET) (void) 
+void fpresetF77 (void) 
 { 
   fenv_t env; 
   feholdexcept(&env); 
@@ -46,8 +56,7 @@ int fperror (void)
   feclearexcept(FE_ALL_EXCEPT);
   return flags != 0;
 }
-
-int F77_FUNC(fperror,FPERROR) (void) 
+int fperrorF77 (void) 
 {
   int flags = fetestexcept(FE_ALL_EXCEPT);
   feclearexcept(FE_ALL_EXCEPT);
--- gj2/trapfpe.c
+++
@@ -1,9 +0,0 @@
-#include <fpu_control.h>
-static void __attribute__ ((constructor))
-trapfpe ()
-{
-  _FPU_SETCW (_FPU_DEFAULT &
-              ~(_FPU_MASK_IM |_FPU_MASK_UM | _FPU_MASK_ZM | _FPU_MASK_OM));
-}
-
-
