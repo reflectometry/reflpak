3099e1235f588d255d516e5ce19cb206a5de5165
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2003-10-30 15:29:22 +0000

    Make sure that the flags are cleared when the traps are reset
    git-svn-id: svn+ssh://danse.us/reflpak@145 6e772d39-f918-0410-b64e-d616bd391b2c

--- gj2/fperror.c
+++ gj2/fperror.c
@@ -36,18 +36,37 @@ FORTRAN usage:
 #define fperrorF77 F77_FUNC(fperror,FPERROR)
 #define fpresetF77 F77_FUNC(fpreset,FPRESET)
 
+#define USEFUL_FLAGS FE_DIVBYZERO|FE_UNDERFLOW|FE_OVERFLOW|FE_INVALID
 void fpreset (void) 
 { 
   fenv_t env; 
   feholdexcept(&env); 
+  feclearexcept(USEFUL_FLAGS);
 }
 void fpresetF77 (void) 
 { 
   fenv_t env; 
   feholdexcept(&env); 
+  feclearexcept(USEFUL_FLAGS);
 }
 
-#define USEFUL_FLAGS FE_DIVBYZERO|FE_UNDERFLOW|FE_OVERFLOW|FE_INVALID
+#if 0
+void fpreport (int flags)
+{
+  if (flags != 0) {
+    if (flags&FE_DIVBYZERO) printf("DIVBYZERO\n");
+    flags &= ~FE_DIVBYZERO;
+    if (flags&FE_UNDERFLOW) printf("UNDERFLOW\n");
+    flags &= ~FE_UNDERFLOW;
+    if (flags&FE_OVERFLOW) printf("OVERFLOW\n");
+    flags &= ~FE_OVERFLOW;
+    if (flags&FE_INVALID) printf("INVALID\n");
+    flags &= ~FE_INVALID;
+    if (flags != 0) printf("flags still contains %x\n",flags);
+  }
+}
+#endif
+
 int fperror (void) 
 {
   int flags = fetestexcept(USEFUL_FLAGS);
