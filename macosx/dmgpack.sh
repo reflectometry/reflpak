e261dcbda250ed7a792dbdc1c8d9e55599d647e0
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2004-04-09 19:03:17 +0000

    allow trailing / on directory use mount/umount instead of hdid so that gid is set right allow relative path to source files and directories
    git-svn-id: svn+ssh://danse.us/reflpak@307 6e772d39-f918-0410-b64e-d616bd391b2c

--- macosx/dmgpack.sh
+++ macosx/dmgpack.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#! /bin/bash
 
 # dmgpack volume [file|dir]+
 #
@@ -20,29 +20,43 @@
 
 test $# -lt 2 && echo "usage: $0 diskname [file|dir]+" && exit 1
 set -x
-NAME=${1%.dmg} ; shift
+NAME="${1%.dmg}" ; shift
 DISK=/tmp/dmgpack$$.dmg
-COMPRESSED=$NAME.dmg
-VOLUME=$NAME
+COMPRESSED="$NAME.dmg"
+VOLUME="$NAME"
+
 # compute needed image size; scale it by 10%
-SIZE=$(du -ck $* | tail -1 | sed -e 's/ *total//')
+SIZE=$(du -ck "$@" | tail -1 | sed -e 's/ *total//')
 SIZE=$(echo $SIZE*11/10 | bc)
 test $SIZE -lt 4200 && SIZE=4200
+
 # create the disk
 rm -f $DISK
 hdiutil create -size ${SIZE}k $DISK -layout NONE
-# create a file system on the disk
-DEVICE=$(hdid -nomount $DISK)
-newfs_hfs -v $VOLUME $DEVICE
-hdiutil eject $DEVICE
+
+# create a file system on the disk; last line of output is
+# the device on which the disk was attached.
+DEVICE=$(hdiutil attach $DISK -nomount | tail -1)
+newfs_hfs -v "$VOLUME" $DEVICE
+
+# mount the file system
+mkdir $DISK-mount
+mount -t hfs $DEVICE $DISK-mount || (echo "mount $DISK-mount failed" && exit 1)
+
 # copy stuff to the disk and fixup resource forks
-hdid $DISK
-for f in $*; do 
-	ditto -rsrc $f /Volumes/$VOLUME/$f; 
-	test -d $f && /System/Library/CoreServices/FixupResourceForks /Volumes/$VOLUME/$f
+for f in "$@"; do 
+    f=${f%/}		;# strip trailing /
+    dest="$DISK-mount/${f##*/}"
+    ditto -rsrc "$f" "$dest" 
+    test -d "$f" && /System/Library/CoreServices/FixupResourceForks "$dest"
 done
+
+# eject the disk
+umount $DISK-mount
+rmdir $DISK-mount
 hdiutil eject $DEVICE
+
 # compress the disk and make it read only
-rm -f $COMPRESSED
-hdiutil convert -format UDZO $DISK -o $COMPRESSED
+rm -f "$COMPRESSED"
+hdiutil convert -format UDZO $DISK -o "$COMPRESSED"
 rm -f $DISK
