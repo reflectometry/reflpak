9d7123338893c50f80240e56bfae28827f356f85
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2004-07-12 18:25:31 +0000

    Just handle simple data for now---polarized coming soon.
    git-svn-id: svn+ssh://danse.us/reflpak@336 6e772d39-f918-0410-b64e-d616bd391b2c

--- reflred/octave/run_send.m
+++ reflred/octave/run_send.m
@@ -1,14 +1,10 @@
+# Send an x,y,dy structure to tcl
 function run_send(name, val)
   if isempty(val)
     send(sprintf(name,'x'),[]);
     send(sprintf(name,'y'),[]);
     send(sprintf(name,'dy'),[]);
     send(['run_send clear ',sprintf(name,'')]);
-  elseif !isempty(val.polarization)
-    run_send(sprintf(name,'A%s'),val.A);
-    run_send(sprintf(name,'B%s'),val.B);
-    run_send(sprintf(name,'C%s'),val.C);
-    run_send(sprintf(name,'D%s'),val.D);
   else
     send(sprintf(name,'x'),val.x);
     send(sprintf(name,'y'),val.y);
