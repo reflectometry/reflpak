c84537d2e167bf5179df49bc25fb9c44fa3269b5
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2005-05-13 23:01:01 +0000

    Add fextract and fdivide
    git-svn-id: svn+ssh://danse.us/reflpak@382 6e772d39-f918-0410-b64e-d616bd391b2c

--- meshplot/refl.c
+++ meshplot/refl.c
@@ -41,6 +41,28 @@ void mx_transpose(int n, int m, mxtype *a, mxtype *b)
   }
 }
 
+/* 0-origin dense column extraction */
+void mx_extract_columns(int m, int n, const mxtype *a,
+			int column, int width, mxtype *b)
+{
+  int i, j;
+  int idx = 0;
+  a += column;
+  for (i=0; i < m; i++) {
+    for (j=0; j < width; j++) b[idx++] = a[j];
+    a += n;
+  }
+}
+
+void mx_divide_columns(int m, int n, mxtype *M, const mxtype *y)
+{
+  int i, j;
+  for (i=0; i < m; i++) {
+    for (j=0; j < n; j++) M[j] /= y[i];
+    M += n;
+  }
+}
+
 
 /* Build a mesh from the scan of linear detector readings.
  *
@@ -194,10 +216,96 @@ build_Qmesh(int n, int m, PReal lambda,
 
 /* ================================================================ */
 
+static int
+fdivide(ClientData junk, Tcl_Interp *interp, 
+	 int argc, Tcl_Obj *CONST argv[])
+{
+  int m,n;
+  const PReal *y;
+  PReal *M;
+  const char *name;
+
+  /* Interpret args */
+  if (argc != 5) {
+    Tcl_SetResult( interp,
+		   "wrong # args: should be \"fdivide m n M y\"",
+		   TCL_STATIC);
+    return TCL_ERROR;
+  }
+  if (Tcl_GetIntFromObj(interp,argv[1],&m) != TCL_OK 
+      || Tcl_GetIntFromObj(interp,argv[2],&n) != TCL_OK) {
+    return TCL_ERROR;
+  }
+
+  /* Get data vector */
+  name = Tcl_GetString(argv[3]);
+  M = get_unshared_tcl_vector(interp,name,"fdivide","M",m*n);
+  if (M == NULL) return TCL_ERROR;
+
+  name = Tcl_GetString(argv[4]);
+  y = get_tcl_vector(interp,name,"fdivide","y",m);
+  if (y == NULL) return TCL_ERROR;
+
+  /* Extract data */
+  mx_divide_columns(m,n,M,y);
+
+  return TCL_OK;
+}
+
+static int
+fextract(ClientData junk, Tcl_Interp *interp, 
+	 int argc, Tcl_Obj *CONST argv[])
+{
+  int m,n,column,width=1;
+  const PReal *x;
+  PReal *y;
+  Tcl_Obj *yobj;
+  const char *name;
+
+  /* Interpret args */
+  if (argc != 5 && argc != 6) {
+    Tcl_SetResult( interp,
+		   "wrong # args: should be \"fextract m n x column ?width\"",
+		   TCL_STATIC);
+    return TCL_ERROR;
+  }
+  if (Tcl_GetIntFromObj(interp,argv[1],&m) != TCL_OK 
+      || Tcl_GetIntFromObj(interp,argv[2],&n) != TCL_OK
+      || Tcl_GetIntFromObj(interp,argv[4],&column) != TCL_OK
+      || (argc==6 && Tcl_GetIntFromObj(interp,argv[5],&width) != TCL_OK)) {
+    return TCL_ERROR;
+  }
+
+  if (column+width > n || column < 0 || width < 1) {
+    Tcl_SetResult( interp,
+		   "fextract: requesting columns outside matrix",
+		   TCL_STATIC);
+    return TCL_ERROR;
+  }
+
+
+  /* Get data vector */
+  name = Tcl_GetString(argv[3]);
+  x = get_tcl_vector(interp,name,"fextract","x",m*n);
+  if (x == NULL) return TCL_ERROR;
+
+  /* Build return vector */
+  yobj = Tcl_NewByteArrayObj(NULL,0);
+  if (!yobj) return TCL_ERROR;
+  Tcl_SetObjResult(interp,yobj);
+  y = (PReal *)Tcl_SetByteArrayLength(yobj,m*width*sizeof(PReal));
+  if (!y) return TCL_ERROR;
+
+  /* Extract data */
+  mx_extract_columns(m,n,x,column,width,y);
+
+  return TCL_OK;
+}
+
 /* wrap in-place transpose function */
 static int 
-ftranspose(ClientData junk, Tcl_Interp *interp, int argc, 
-	       Tcl_Obj *CONST argv[])
+ftranspose(ClientData junk, Tcl_Interp *interp, 
+	   int argc, Tcl_Obj *CONST argv[])
 {
   int m,n;
   PReal *x;
@@ -331,5 +439,7 @@ buildmesh(ClientData junk, Tcl_Interp *interp, int argc,
 static void refl_init(Tcl_Interp *interp)
 {
   Tcl_CreateObjCommand( interp, "ftranspose", ftranspose, NULL, NULL );
+  Tcl_CreateObjCommand( interp, "fextract", fextract, NULL, NULL );
+  Tcl_CreateObjCommand( interp, "fdivide", fdivide, NULL, NULL );
   Tcl_CreateObjCommand( interp, "buildmesh", buildmesh, NULL, NULL );
 }
