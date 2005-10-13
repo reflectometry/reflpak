e204b2d5a88ef4436b848f0a40d53be10f1e9685
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2005-10-13 15:36:34 +0000

    Restructure distribution servers to support DAVE build
    git-svn-id: svn+ssh://danse.us/reflpak@555 6e772d39-f918-0410-b64e-d616bd391b2c

--- release.sh
+++ release.sh
@@ -11,36 +11,52 @@ VERSION="`date +-%Y.%m.%d`"
 # isn't set up for ssh operations.
 
 # Here are my architecture specific build machines:
-BUILD="jazz macng7 h122045 dave"
+irix=jazz
+osx=macng7
+linux3=h122045
+linux4=dave
+win=localhost
+BUILD="$irix $osx $linux3 $linux4"
+
 # Grrr... irix machines need gmake rather than make...
-makejazz="gmake"
+make$irix="gmake"
+
+# Rather than getting gif2png conversion to work under
+# windows, export the problem to a machine with imagemagick
+# and tclsh.
+htmlmachine=h122045
+
 # Each machine has already been set up with a build directory 
 # in ~/cvs/reflfit and the appropriate Makeconf.
 builddir="~/cvs/reflfit"
 
 # The results are stored and shared in the following directories.
 # These may be local or remote since scp doesn't care:
-STORE=jazz:release
-SHARE=webster:software/release
+WEBDIR=webster:software/release
+WEBCP=scp
 
-# The following directory will contain reflpak$VERSION and 
-# reflpak-latest symlinked to relfpak$VERSION.
-SHAREBIN=jazz:bin
+# The following directory will contain $arch/reflpak$VERSION and 
+# a copy of the latest in $arch/reflpak
+# MSYS cp to shared is broken for versions before 1.0.11
+# We are using cygwin's cp instead.
+BINDIR="//charlotte/public/Reflpak"
+BINCP="/c/cygdrive/bin/cp -r"
 
-# Rather than getting gif2png conversion to work under
-# windows, export the problem to a machine with imagemagic
-# and tclsh.
-htmlmachine=h122045
 # =========== End of configuration ============
 
-echo "== local cvs update ============================"
+# Perform CVS updates on all machines
+echo "== cvs update ============================"
 cvs -q update -dP
 for machine in $BUILD; do
     echo;echo "== cvs update on $machine: ===================";
     ssh $machine "cd $builddir && cvs -q update -dP"
 done
-
 echo; echo "Are all files committed that need to be?"
+echo -n "Press y to continue: "
+read ans
+test "$ans" != "y" && exit
+
+# Check release notes
 echo "Are any files not added that should be added?"
 echo "Are the RELEASE-NOTES up to date, and tagged with today's date?"
 ls -l RELEASE-NOTES
@@ -49,15 +65,15 @@ echo -n "Press y to continue: "
 read ans
 test "$ans" != "y" && exit
 
-
-echo; echo "== local build ========================"
-make dist
+# Do the local build
 echo; echo "== build html ========================="
 ssh $htmlmachine "cd $builddir && make html"
 ssh $htmlmachine "cd $builddir && make datadist"
 echo; echo "== build source ======================="
 make srcdist
 
+# Do the remote builds (strictly speaking these could be done
+# in parallel but then we would need to deal with synchronization
 for machine in $BUILD; do
     echo; echo "== build on $machine ========================"
     # if make$machine is a defined variable use it, otherwise use 'make'
@@ -65,47 +81,72 @@ for machine in $BUILD; do
     ssh $machine "cd $builddir && ${!par:-make} && ${!par:-make} dist"
 done
 
-mkdir reflpak$VERSION
-echo; echo "== gather local build results ====================="
-cp release/reflpak$VERSION* reflpak$VERSION
-echo; echo "== gather html ======================="
-scp -r $htmlmachine:$builddir/html reflpak$VERSION
-scp $htmlmachine:$builddir/release/reflpak-data.zip reflpak$VERSION
-for machine in $BUILD; do
-    echo; echo "== gather results from $machine ================="
-    scp "$machine:$builddir/release/reflpak$VERSION*" reflpak$VERSION
-done
-
-echo; echo -n "Update server? [y for yes]: "
+# Do the local build last since you need to type exit in the interpreter
+# when it is complete.
+echo; echo "== local build ========================"
+make && make dist
+echo; echo "Scroll back and check for build errors"
+echo -n "Press y to continue: "
 read ans
 test "$ans" != "y" && exit
 
-echo; echo "== updating $STORE, $SHARE and $SHAREBIN =="
-sed -e"s,@VERSION@,$VERSION,g" < INSTALL >reflpak$VERSION/index.html
-cp RELEASE-NOTES reflpak$VERSION
-tar cjf reflpak$VERSION.tar.bz2 reflpak$VERSION
-scp reflpak$VERSION.tar.bz2 "$STORE"
-scp -r reflpak$VERSION "$SHARE"
-ssh ${SHARE%:*} "cd ${SHARE#*:} && rm reflpak && ln -s reflpak$VERSION reflpak"
-ssh ${SHAREBIN%:*} "cd ${SHAREBIN#*:} && cp $builddir/kit/reflpak reflpak$VERSION && rm reflpak && ln -s reflpak$VERSION reflpak"
-rm -rf reflpak$VERSION
-echo; echo "Please check that $STORE and $SHARE contain what you want"
+# Gather results for the web distribution
+rm -rf web; mkdir web
+echo; echo "== gather local build results ====================="
+cp release/reflpak$VERSION.exe web
+for machine in $BUILD; do
+    echo; echo "== gather results from $machine ================="
+    scp "$machine:$builddir/release/reflpak$VERSION*" web
+done
+sed -e"s,@VERSION@,$VERSION,g" < INSTALL >web/index.html
+cp RELEASE-NOTES web
+echo; echo "== gather html ======================="
+scp -r $htmlmachine:$builddir/html web
+scp $htmlmachine:$builddir/release/reflpak-data.zip web
 
-# Update the windows share with the current version
-# XXX FIXME XXX MSYS cp to shared is broken for versions before 1.0.11
-# We are using cygwin's cp instead.
-echo; echo "Copying to //charlotte/public/Reflpak"
-/c/cygdrive/bin/cp release/reflpak$VERSION.exe //charlotte/public/Reflpak
+# Gather results for the binary server
+rm -rf bin; mkdir bin
+for arch in irix osx linux3 linux4; do
+   mkdir bin/$arch
+   scp $arch:$builddir/kit/reflpak bin/$arch/reflpak$VERSION
+done
+mkdir bin/win
+cp release/reflpak$VERSION.exe bin/win
 
+
+# Check if we should put the current build on the servers
+echo; echo -n "Check web and bin are correct.  Update server? [y for yes]: "
+read ans
+test "$ans" != "y" && exit
+echo; echo "== updating $WEBDIR and $BINDIR =="
+
+echo; echo "Copying to $WEBDIR"
+$WEBCP -r web "$WEBDIR/reflpak$VERSION"
+echo; echo "Copying to $BINDIR"
+$BINCP bin/* "$BINDIR"
+
+# Check if we should make the current build an official release
+echo; echo "Check $WEBDIR and $BINDIR"
 echo; echo -n "Tag the release? [y for yes]: "
 read ans
-test "$ans" = "y" && make tagdist
+if test "$ans" = "y"; then
+    # Tag the cvs tree for the current release
+    make tagdist
 
+    # Make the web release current
+    ssh ${WEBDIR%:*} "cd ${WEBDIR#*:} && rm reflpak && ln -s reflpak$VERSION reflpak"
+
+    # Make the binary release current
+    for arch in irix osx linux3 linux4; do
+	$BINCP bin/$arch/reflpak$VERSION "$BINDIR/$arch/reflpak"
+    done
+    $BINCP bin/win/reflpak$VERSION.exe "$BINDIR/win/reflpak.exe"
+fi
+
+# Make sure the instrument computers are updated.
 echo Update instrument computers, user room software and web.
-echo On a linux-gcc3 box (h122045):
-echo    scp ~/cvs/reflfit/kit/reflpak cg1@andr:bin/reflpak$VERSION
-echo    scp ~/cvs/reflfit/kit/reflpak ng7@ng7refl:bin/reflpak$VERSION
-echo On a linux-gcc4 box (dave):
-echo    scp ~/cvs/reflfit/kit/reflpak ng1@ng1refl:bin/reflpak$VERSION
+echo    scp bin/linux3/reflpak cg1@andr:bin/reflpak$VERSION
+echo    scp bin/linux3/reflpak ng7@ng7refl:bin/reflpak$VERSION
+echo    scp bin/linux4/reflpak ng1@ng1refl:bin/reflpak$VERSION
 echo Also need to point to the latest via symlink.
 echo Let users know a new version is available.
