2c19f1cb2f1a113f0b78e5d4f5b1c7d536f35906
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2005-10-27 19:26:18 +0000

    Move collect/trigger acceleration to <MouseWheel> event.
    git-svn-id: svn+ssh://danse.us/reflpak@584 6e772d39-f918-0410-b64e-d616bd391b2c

--- lib/mousewheel.tcl
+++ lib/mousewheel.tcl
@@ -22,7 +22,7 @@
     switch -- $action {
  	bind {
 	    bind [winfo toplevel $w] <MouseWheel> \
-		{ mousewheel::doevent %W %X %Y %D }
+		{ mousewheel::collect %W %X %Y %D }
 	}
 	step {
 	    return [mousewheel::step $w] 
@@ -34,41 +34,33 @@
  }
 
 
+
  namespace eval mousewheel {
 
-    # Convert unix Button-4/Button-5 to MouseWheel events.  Collect events
-    # in 200 ms intervals so that we can do speed estimates.
-    bind all <Button-4> [namespace code {collect  1 %W %X %Y %x %y}]
-    bind all <Button-5> [namespace code {collect -1 %W %X %Y %x %y}]
     variable afterid {}
     variable factor 0
 
-    proc collect {n W X Y x y} {
+    proc collect {W X Y D} {
 	variable factor
 	variable afterid
 	if {$afterid eq {}} {
-	    set factor $n
-	    set afterid [after 200 [namespace code "trigger $W $X $Y $x $y"]]
+	    set factor $D
+	    set afterid [after 200 [namespace code "trigger $W $X $Y"]]
 	} else {
-	    incr factor $n
+	    incr factor $D
 	}
     }
-    proc trigger {W X Y x y} {
-	variable afterid {}
-        variable factor
-	event generate $W <MouseWheel> \
-	    -delta [expr {$factor*120}] -rootx $X -rooty $Y -x $x -y $y
-    }
-
 
     # Convert <MouseWheel> to <<Wheel>> event.
-    proc doevent {W X Y D} {
+    proc trigger {W X Y} {
+	variable afterid {}
+        variable factor
 	set w [winfo containing -displayof $W $X $Y]
 	if { $w ne "" } {
 	    set x [expr {$X-[winfo rootx $w]}]
 	    set y [expr {$Y-[winfo rooty $w]}]
 	    variable delta
-	    set delta($w) $D
+	    set delta($w) $factor
 	    event generate $w <<Wheel>> -rootx $X -rooty $Y -x $x -y $y
 	}
     }
@@ -79,3 +71,14 @@
 	return [expr {int(pow($delta($w)/120,3))}]
     }
  }
+
+
+ # Convert unix Button-4/Button-5 to MouseWheel events.  Collect events
+ # in 200 ms intervals so that we can do speed estimates.
+ bind all <Button-4> { 
+    event generate %W <MouseWheel> -delta 120 -rootx %X -rooty %Y -x %x -y %y
+ }
+ bind all <Button-5> {
+    event generate %W <MouseWheel> -delta -120 -rootx %X -rooty %Y -x %x -y %y
+ }
+
