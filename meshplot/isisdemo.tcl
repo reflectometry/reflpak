e03e75965f7baf0511a9f14e3b9e985f109a496b
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2005-12-10 00:54:10 +0000

    debugging stuff...
    git-svn-id: svn+ssh://danse.us/reflpak@611 6e772d39-f918-0410-b64e-d616bd391b2c

--- meshplot/isisdemo.tcl
+++ meshplot/isisdemo.tcl
@@ -1,13 +1,16 @@
 namespace eval isis {
+variable DATADIR /home/pkienzle/data/test
 
 proc demo {{mesh_style QxQz}} {
+    puts "starting demo"
     set w [reflplot::plot_window]
-    set DATADIR /home/pkienzle/data/test
+    variable DATADIR
     isis::read_data [file join $DATADIR isis SRF25115.RAW] rec1
     isis::read_data [file join $DATADIR isis SRF25149.RAW] rec2
 
-#    plot2d center $w 15
-#    plot2d add $w { rec1 rec2 }
+return
+    plot2d center $w 15
+    plot2d add $w { rec1 rec2 }
 }
 
 proc read_data { file id } {
@@ -24,6 +27,7 @@ proc read_data { file id } {
     set rec(pixelwidth) [$fid pixelwidth]
     set rec(points) [$fid Nt]
     set rec(pixels) [$fid Ny]
+return
     set rec(lambda) [$fid lambda]
     set rec(psddata) [$fid I]
     set rec(psderr) [$fid dI]
