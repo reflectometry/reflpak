4d3be0ebd77da4ecd79695f510662c741f944a1c
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2005-12-19 21:08:42 +0000

    support frame plot
    git-svn-id: svn+ssh://danse.us/reflpak@625 6e772d39-f918-0410-b64e-d616bd391b2c

--- meshplot/isisdemo.tcl
+++ meshplot/isisdemo.tcl
@@ -5,13 +5,17 @@ proc demo {{mesh_style QxQz}} {
     puts "starting demo"
     set w [reflplot::plot_window]
     variable DATADIR
-    # isis::read_data [file join $DATADIR isis SRF25115.RAW] rec1000
+    #isis::read_data [file join $DATADIR isis SRF25115.RAW] rec2000
     isis::read_data [file join $DATADIR isis SRF25149.RAW] rec2000
 
     plot2d transform $w $mesh_style
-    plot2d center $w 15
+    plot2d center $w 23.5
     plot2d add $w { rec2000 }
+    $w configure -vmin 0.002 -vmax 2.
     reflplot::monitor rec2000
+    reflplot::frameplot rec2000
+    reflplot::setframe
+    tkcon show
 }
 
 proc read_data { file id } {
@@ -24,17 +28,20 @@ proc read_data { file id } {
     set rec(file) [file root $file]
     set rec(legend) [file root [file tail $file]]
     set rec(TOF) 1
-    set rec(A) 1.
-    set rec(B) 2.
+    set rec(A) 1.5
+    set rec(B) 3.0
     set rec(distance) [$fid distance]
     set rec(pixelwidth) [$fid pixelwidth]
-    set rec(points) [$fid Nt]
     set rec(pixels) [$fid Ny]
+    set rec(column,monitor_raw) [$fid monitor_raw]
+    set rec(column,monitor_raw_lambda) [$fid monitor_raw_lambda]
+
+    $fid proportional_binning 0.55 5.8 1.
+
+    set rec(points) [$fid Nt]
     set rec(lambda) [$fid lambda_edges]
     set rec(column,lambda) [$fid lambda]
     set rec(column,monitor) [$fid monitor]
-    set rec(column,monitor_raw) [$fid monitor_raw]
-    set rec(column,monitor_raw_lambda) [$fid monitor_raw_lambda]
     set rec(psdraw) [$fid counts]
     set rec(psddata) [$fid I]
     set rec(psderr) [$fid dI]
