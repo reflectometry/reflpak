983e3a11a1b506082a4885d4c796ac8bc732297a
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2006-01-17 15:00:40 +0000

    Define slice API in header; stddef needed for size_t;
    git-svn-id: svn+ssh://danse.us/reflpak@656 6e772d39-f918-0410-b64e-d616bd391b2c

--- meshplot/mx.h
+++ meshplot/mx.h
@@ -2,6 +2,7 @@
 #define PRECISION float
 #endif
 
+#include <stddef.h>
 #define mxtype PRECISION
 
 void mx_transpose(int m, int n, mxtype *a, mxtype *b);
@@ -14,3 +15,7 @@ void mxdx_divide_columns(int m, int n, mxtype *M, mxtype *dM, const mxtype *y);
 void mx_quad_search(int n, int m, mxtype *x, mxtype *y, 
 		    int *xidx, int *yidx,
 		    double x1, double y1, double x2, double y2);
+
+int mx_slice(const size_t m, const size_t n, 
+	     const mxtype x[], const mxtype y[],
+	     const mxtype L[], size_t Nidx, size_t idx[]);
