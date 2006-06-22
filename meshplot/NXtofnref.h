f302c60bc469c48740011229050b7272a165a7c5
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2006-06-22 23:41:20 +0000

    reverse dimensions in 3D array (commented out)
    git-svn-id: svn+ssh://danse.us/reflpak@746 6e772d39-f918-0410-b64e-d616bd391b2c

--- meshplot/NXtofnref.h
+++ meshplot/NXtofnref.h
@@ -77,10 +77,10 @@ public:
  * He does not remember who is the original author.
  */
 template <class Real> void 
-transpose(int n, int m, Real a[], Real b[])
+transpose(int n, int m, Real a[], Real *b = NULL)
 {
   int size = m*n;
-  if(b!=a){ /* out of place transpose */
+  if(b!=NULL && b!=a){ /* out of place transpose */
     Real *bmn, *aij, *anm;
     bmn = b + size; /*b+n*m*/
     anm = a + size;
@@ -102,6 +102,27 @@ transpose(int n, int m, Real a[], Real b[])
     }
   }
 }
+
+#if 0
+// Swap between fortran and C indexing in a 3 dimensional array.
+// Should be a clever way to do this in one pass, but instead
+// we transpose each matrix separately then swap the ordering of
+// the channels. 
+template <class Real> void 
+c_to_fortran_indexing(int n, int m, int l, Real a[], Real *b = NULL)
+{
+  if (b==NULL) b=a;
+  transpose(l,n*m,a,b);
+  for (int i=0; i < l; i++) transpose(n,m,b+i*n*m,b+i*n*m);
+}
+template <class Real> void 
+fortran_to_c_indexing(int n, int m, int l, Real a[], Real *b = NULL)
+{
+  if (b==NULL) b=a;
+  for (int i=0; i < l; i++) transpose(m,n,a+i*n*m,b+i*n*m);
+  transpose(n*m,l,b,b);
+}
+#endif
 
 // rebin_counts(Nx, x, Ix, dIx, Ny, y, Iy, dIy)
 // Rebin from the bin edges in x to the bin edges in y where
