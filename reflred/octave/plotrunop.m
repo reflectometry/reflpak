68f9bb2eadb3b10f211972330d342e1162281bbf
pkienzle <pkienzle@6e772d39-f918-0410-b64e-d616bd391b2c>
2007-06-18 17:57:57 +0000

    Newer versions of octave no longer have gset
    git-svn-id: svn+ssh://danse.us/reflpak@815 6e772d39-f918-0410-b64e-d616bd391b2c

--- reflred/octave/plotrunop.m
+++ reflred/octave/plotrunop.m
@@ -1,33 +1,23 @@
+## plotrunop(result,in1,in2)
+## Plot input values and results for a run operation.  Supports unary
+## operations if only one input value is provided in addition to the
+## result.
 function plotrunop(run, run1, run2)
 
+  axis;
   if nargin == 3
 
-    graw("reset\n");
-
-    global __multiplot_mode__;
-    __multiplot_mode__ = 1;
-    gset multiplot;
-    gset origin 0, 0
-    gset size 1, 1
-    graw("clear\n");
-    gset origin 0, 0.5;
-    gset size 0.5, 0.4;
-    axis;
+    subplot(221);
     xlabel(run1.xlabel);
     ylabel(run1.ylabel);
     title(run1.title);
     errorbar(run1.x,run1.y,run1.dy,'~b;;');
 
-    gset origin 0.5, 0.5;
-    gset size 0.5, 0.4;
-    axis;
+    subplot(222);
     xlabel(run2.xlabel);
     ylabel(run2.ylabel);
     title(run2.title);
     errorbar(run2.x,run2.y,run2.dy,'~r;;');
-
-    gset origin 0, 0;
-    gset size 1.0, 0.4;
 
   elseif nargin == 2
     subplot(211);
