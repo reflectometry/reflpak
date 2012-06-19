bb2e31bb1183e89a9823927a28999a4d2cd11051
Paul Kienzle <pkienzle@nist.gov>
2012-06-19 12:44:10 -0400

    fix sortrows; provide init.d server

--- octave_server/start_server.sh
+++ octave_server/start_server.sh
@@ -7,6 +7,6 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 ROOT="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-# Start octave in the script directory
+# Start octave in the script directory, with sortrows on the path
 cd "$ROOT"
-echo "server(1515,'129.6.12[123].*')" | octave -q
+echo "server(1515,'129.6.12[0123].*')" | octave -q -p "$ROOT"
