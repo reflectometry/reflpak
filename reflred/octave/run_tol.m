37f47658f5b3d51444931696895c0dec6151db02
Brian Benjamin Maranville <brian.maranville@nist.gov>
2014-07-08 12:25:09 -0400

    reverting to tolerance = fixed 10*eps;

--- reflred/octave/run_tol.m
+++ reflred/octave/run_tol.m
@@ -9,11 +9,12 @@
 ## points, but that doesn't work very well if points are repeated
 ## or if there is only one point.
 function tol = run_tol(x1, x2, fuzz)
-    d1 = diff(x1(x1>=x2(1) & x1<=x2(end)));
-    d2 = diff(x2(x2>=x2(1) & x2<=x2(end)));
-    if length(d1)>1 && length(d2)>1, then
-        tol = min([median(d1),median(d2)])/3.;
-    else
-        tol = 10*eps;
-    end
+    tol = 100*eps;
+#    d1 = diff(x1(x1>=x2(1) & x1<=x2(end)));
+#    d2 = diff(x2(x2>=x2(1) & x2<=x2(end)));
+#    if length(d1)>1 && length(d2)>1, then
+#        tol = min([median(d1),median(d2)])/3.;
+#    else
+#        tol = 10*eps;
+#    end
 endfunction
